5e047c5947beeca082c7b6204391c13316ad6423
korbek100 <korbek100@gmail.com>
2018-01-08 23:17:47 -0300

    Initial commit

---
+++ LoginFacebook/LoginFacebook/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  LoginFacebook
+//
+//  Created by Henry AT on 8/25/15.
+//  Copyright (c) 2015 Apps4s. All rights reserved.
+//
+
+#ifndef LoginFacebook_Bridging_Header_h
+#define LoginFacebook_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
